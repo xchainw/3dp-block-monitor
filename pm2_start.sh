7b7e66064d6d540da2cc8d967248b2978ab301d4
xchainw <xchainw@gmail.com>
2025-07-03 12:37:41 +0800

    feat: add pm2 start script

---
+++ pm2_start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Starting 3dp-block-monitor-app"
+pm2 start npm --name "3dp-block-monitor-app" -- start
+
+echo "Starting 3dp-block-monitor-web"
+pm2 start npm --name "3dp-block-monitor-web" -- run web
